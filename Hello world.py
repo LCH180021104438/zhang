4f0047596379cf1e86faac9e4e28ed2fcc309d9e
LCH180021104438 <1115744810@qq.com>
2020-05-16 20:39:32 +0800

    Update hello.py . add a leijia file

--- Hello world.py
+++ Hello world.py
@@ -1,5 +1,6 @@
 print("Hello world")
 print("世界，你好")
 print("Python学习第一案例Hello world")
+print("Hello xiaozhang")
 
 
---
+++ 累加函数.py
@@ -0,0 +1,7 @@
+def sum(N):
+    sum1=0
+    for i in range(N+1):
+        sum1=sum1+i
+    print(sum1)
+N=eval(input())
+sum(N)
